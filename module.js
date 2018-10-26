f52b3f2cc96d2b8ef15a439083d5d8a0cf559bd7
Alexander <alexxandreas@gmail.com>
2018-10-26 18:00:43 +0000

    -

--- module.js
+++ module.js
@@ -4668,18 +4668,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var lodash_map__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash_map__WEBPACK_IMPORTED_MODULE_0__);
 
 
-// (function() {
-//     const start = () => {
-//         console.log('ZWayConsole module.js start');
-//         const a = _.map([1,2], val => val+1);
-//         console.log(`ZWayConsole module.js ${a.join(',')}`);
-//     }
-//     const stop = () => {
-//         console.log('ZWayConsole module.js stop');
-//     }
-//     start();
-//     return { stop };
-// })();
 var start = function start() {
   console.log('ZWayConsole module.js start');
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import _ from 'lodash';
 
-// (function() {
-//     const start = () => {
-//         console.log('ZWayConsole module.js start');
-//         const a = _.map([1,2], val => val+1);
-//         console.log(`ZWayConsole module.js ${a.join(',')}`);
-//     }
-    
-//     const stop = () => {
-//         console.log('ZWayConsole module.js stop');
-//     }
-    
-//     start();
-//     return { stop };
-// })();
-
 const start = () => {
     console.log('ZWayConsole module.js start');
     const a = _.map([1,2], val => val+1);
